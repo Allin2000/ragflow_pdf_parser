4b8ad9ecd4285e13d291f6bab03d69fce9f53a01
Allin2000 <serendipitousx@outlook.com>
2024-07-14 09:35:40 +0800

    paper second

--- rag/app/paper.py
+++ rag/app/paper.py
@@ -20,14 +20,12 @@ from io import BytesIO
 import base64
 
 import pandas as pd
-from collections import defaultdict
 
 
 class Pdf(PdfParser):
     def __init__(self):
         self.model_speciess = ParserType.PAPER.value
         super().__init__()
-        # self.all_boxes = []  
 
     @staticmethod
     def sort_X_by_page(arr, threashold):
@@ -43,6 +41,7 @@ class Pdf(PdfParser):
                     arr[j] = arr[j + 1]
                     arr[j + 1] = tmp
         return arr
+    
 
     # @staticmethod
     # def process_pdf_data(pdf_data):
@@ -91,59 +90,124 @@ class Pdf(PdfParser):
 
     #     self.boxes = sorted_boxes
 
-    def sort_boxes(self):
-        page_groups = defaultdict(list)
-        for item in self.boxes:
-            page_groups[item['page_number']].append(item)
+    # def sort_boxes(self):
+    #     page_groups = defaultdict(list)
+    #     for item in self.boxes:
+    #         page_groups[item['page_number']].append(item)
 
-        sorted_boxes = []
+    #     sorted_boxes = []
 
-        for page, items in page_groups.items():
-            if page == 1:
-                pre_keyword_boxes = []
-                post_keyword_boxes = []
-                keyword_found = False
+    #     for page, items in page_groups.items():
+    #         if page == 1:
+    #             pre_keyword_boxes = []
+    #             post_keyword_boxes = []
+    #             keyword_found = False
 
-                for item in items:
-                    if not keyword_found:
-                        if item['text'].strip().lower().startswith(("关键词", "keywords")):
-                            keyword_found = True
-                            pre_keyword_boxes.append(item)
-                        else:
-                            pre_keyword_boxes.append(item)
-                    else:
-                        post_keyword_boxes.append(item)
+    #             for item in items:
+    #                 if not keyword_found:
+    #                     if item['text'].strip().lower().startswith(("关键词", "keywords")):
+    #                         keyword_found = True
+    #                         pre_keyword_boxes.append(item)
+    #                     else:
+    #                         pre_keyword_boxes.append(item)
+    #                 else:
+    #                     post_keyword_boxes.append(item)
 
-                pre_keyword_boxes.sort(key=lambda x: x['top'])
+    #             pre_keyword_boxes.sort(key=lambda x: x['top'])
 
-                x0_groups = defaultdict(list)
-                for item in post_keyword_boxes:
-                    x0_group_key = item['x0'] // 10
-                    x0_groups[x0_group_key].append(item)
+    #             x0_groups = defaultdict(list)
+    #             for item in post_keyword_boxes:
+    #                 x0_group_key = item['x0'] // 10
+    #                 x0_groups[x0_group_key].append(item)
 
-                for x0_key, x0_items in x0_groups.items():
-                    x0_items.sort(key=lambda x: x['top'])
+    #             for x0_key, x0_items in x0_groups.items():
+    #                 x0_items.sort(key=lambda x: x['top'])
 
-                sorted_boxes.extend(pre_keyword_boxes)
-                for x0_key, items in x0_groups.items():
-                    sorted_boxes.extend(items)
-            else:
-                x0_groups = defaultdict(list)
-                for item in items:
-                    x0_group_key = item['x0'] // 10
-                    x0_groups[x0_group_key].append(item)
+    #             sorted_boxes.extend(pre_keyword_boxes)
+    #             for x0_key, items in x0_groups.items():
+    #                 sorted_boxes.extend(items)
+    #         else:
+    #             x0_groups = defaultdict(list)
+    #             for item in items:
+    #                 x0_group_key = item['x0'] // 10
+    #                 x0_groups[x0_group_key].append(item)
 
-                for x0_key, x0_items in x0_groups.items():
-                    x0_items.sort(key=lambda x: x['top'])
+    #             for x0_key, x0_items in x0_groups.items():
+    #                 x0_items.sort(key=lambda x: x['top'])
 
-                for x0_key, items in x0_groups.items():
-                    sorted_boxes.extend(items)
+    #             for x0_key, items in x0_groups.items():
+    #                 sorted_boxes.extend(items)
 
-        self.boxes = sorted_boxes
+    #     self.boxes = sorted_boxes
 
     # def save_boxes_to_csv(self, file_name):
     #     df = pd.DataFrame(self.boxes)
     #     df.to_csv(file_name, index=False)
+ 
+    def sort_boxes(self):
+        # 初始化两个空的列表
+        list1 = []
+        list2 = []
+
+        self.boxes= sorted(self.boxes, key=lambda x: x['top'])
+
+
+        keywords = ("摘要", "abstract", "关键词", "key words")
+
+        # Find the index
+        # Find all indices
+        indices = []
+        for i, item in enumerate(self.boxes):
+            if item['page_number'] == 1 and any(item['text'].strip().lower().startswith(kw) for kw in keywords):
+                indices.append(i)
+
+        # Split the list into two sublists
+        if len(indices)>0:
+            list1 = self.boxes[:indices[-1]]
+            list2 = self.boxes[indices[-1]:]
+        else:
+            # If no index is found, add all data to the first list
+            list2 = self.boxes
+
+        sorted_list1 = sorted(list1, key=lambda x: x['top'])
+
+
+
+        list2_df = pd.DataFrame(list2)
+
+        # Drop rows with NaN values in 'x0'
+        list2_df = list2_df.dropna(subset=['x0'])
+
+    
+
+        sorted_by_page_number = list2_df.sort_values(by='page_number')
+
+        sorted_list = []
+
+        for page, group in sorted_by_page_number.groupby('page_number'):
+            group['x0_group'] = pd.cut(group['x0'], bins=range(0, int(group['x0'].max()) + 50, 50), right=False)
+            sorted_group = group.sort_values(by=['x0_group', 'top'])
+                   # Drop rows with NaN values in 'x0'
+            sorted_group = sorted_group.dropna(subset=['x0_group'])
+            sorted_list.append(sorted_group)
+
+        # for i in sorted_list:
+        #     print(i[['page_number', 'x0', 'x0_group']])
+
+        sorted_by_page_number = pd.concat(sorted_list)
+
+
+        df = sorted_by_page_number.drop('x0_group', axis=1)
+
+        df = df.fillna('')
+
+        list2 = df.to_dict('records')
+
+        self.boxes = sorted_list1+list2
+
+    def save_boxes_to_csv(self, file_name):
+        df = pd.DataFrame(self.boxes)
+        df.to_csv(file_name, index=False)
 
     def __call__(self, filename, binary=None, from_page=0,
                  to_page=100000, zoomin=3, callback=None):
@@ -164,9 +228,10 @@ class Pdf(PdfParser):
         print("layouts:", timer() - start)
         self._table_transformer_job(zoomin)
         callback(0.68, "Table analysis finished")
-        self._text_merge()
+        # self._text_merge()
         tbls = self._extract_table_figure(True, zoomin, True, True)
 
+        
         # self.all_boxes += self.boxes
 
         # self.save_boxes_to_csv('boxes.csv')
@@ -175,6 +240,7 @@ class Pdf(PdfParser):
         #     print(i)
 
         # print("tttttttttttttttttttttttttttttt")
+
 
         column_width = np.median([b["x1"] - b["x0"] for b in self.boxes])
         self._concat_downward()
@@ -187,13 +253,10 @@ class Pdf(PdfParser):
                   self.page_images[0].size[0] / zoomin / 2)
             self.boxes = self.sort_X_by_page(self.boxes, column_width / 2)
 
-        # self.all_boxes += self.boxes
-
-        # Sort the boxes by columns
-        # self.all_boxes = self.process_pdf_data(self.all_boxes)
 
         self.sort_boxes()
 
+        
         # self.save_boxes_to_csv('boxes.csv')
         
         # self.all_boxes = self.all_boxes[0:100]
@@ -202,76 +265,96 @@ class Pdf(PdfParser):
 
         # print("tttttttttttttttttttttttttttttt")
 
+        self.save_boxes_to_csv('boxes.csv')
+        
+
         for b in self.boxes:
             b["text"] = re.sub(r"([\t 　]|\u3000){2,}", " ", b["text"].strip())
 
-        def _begin(txt):
-            return re.match(
-                "[0-9. 一、i]*(introduction|abstract|摘要|引言|keywords|key words|关键词|background|背景|目录|前言|contents)",
-                txt.lower().strip())
 
-        if from_page > 0:
-            return {
-                "title": "",
-                "authors": "",
-                "abstract": "",
-                "sections": [(b["text"] + self._line_tag(b, zoomin), b.get("layoutno", "")) for b in self.boxes if
-                             re.match(r"(text|title)", b.get("layoutno", "text"))],
-                "tables": tbls
-            }
-        # get title and authors
-        title = ""
-        authors = []
-        i = 0
-        while i < min(32, len(self.boxes) - 1):
-            b = self.boxes[i]
-            i += 1
-            if b.get("layoutno", "").find("title") >= 0:
-                title = b["text"]
-                if _begin(title):
-                    title = ""
-                    break
-                for j in range(3):
-                    if _begin(self.boxes[i + j]["text"]):
-                        break
-                    authors.append(self.boxes[i + j]["text"])
-                    break
-                break
-        # get abstract
-        abstr = ""
-        i = 0
-        while i + 1 < min(32, len(self.boxes)):
-            b = self.boxes[i]
-            i += 1
-            txt = b["text"].lower().strip()
-            if re.match("(abstract|摘要)", txt):
-                if len(txt.split(" ")) > 32 or len(txt) > 64:
-                    abstr = txt + self._line_tag(b, zoomin)
-                    break
-                txt = self.boxes[i]["text"].lower().strip()
-                if len(txt.split(" ")) > 32 or len(txt) > 64:
-                    abstr = txt + self._line_tag(self.boxes[i], zoomin)
-                i += 1
-                break
-        if not abstr:
-            i = 0
 
-        callback(
-            0.8, "Page {}~{}: Text merging finished".format(
-                from_page, min(
-                    to_page, self.total_page)))
+        # def _begin(txt):
+        #     return re.match(
+        #         "[0-9. 一、i]*(introduction|abstract|摘要|引言|keywords|key words|关键词|background|背景|目录|前言|contents)",
+        #         txt.lower().strip())
+
+        # if from_page > 0:
+        #     return {
+        #         "title": "",
+        #         "authors": "",
+        #         "abstract": "",
+        #         "sections": [(b["text"] + self._line_tag(b, zoomin), b.get("layoutno", "")) for b in self.boxes if
+        #                      re.match(r"(text|title)", b.get("layoutno", "text"))],
+        #         "tables": tbls
+        #     }
+        # # get title and authors
+        # title = ""
+        # authors = []
+        # i = 0
+        # while i < min(32, len(self.boxes) - 1):
+        #     b = self.boxes[i]
+        #     i += 1
+        #     if b.get("layoutno", "").find("title") >= 0:
+        #         title = b["text"]
+        #         if _begin(title):
+        #             title = ""
+        #             break
+        #         for j in range(3):
+        #             if _begin(self.boxes[i + j]["text"]):
+        #                 break
+        #             authors.append(self.boxes[i + j]["text"])
+        #             break
+        #         break
+        # # get abstract
+        # abstr = ""
+        # i = 0
+        # while i + 1 < min(32, len(self.boxes)):
+        #     b = self.boxes[i]
+        #     i += 1
+        #     txt = b["text"].lower().strip()
+        #     if re.match("(abstract|摘要)", txt):
+        #         if len(txt.split(" ")) > 32 or len(txt) > 64:
+        #             abstr = txt + self._line_tag(b, zoomin)
+        #             break
+        #         txt = self.boxes[i]["text"].lower().strip()
+        #         if len(txt.split(" ")) > 32 or len(txt) > 64:
+        #             abstr = txt + self._line_tag(self.boxes[i], zoomin)
+        #         i += 1
+        #         break
+        # if not abstr:
+        #     i = 0
+
+        # callback(
+        #     0.8, "Page {}~{}: Text merging finished".format(
+        #         from_page, min(
+        #             to_page, self.total_page)))
+        # for b in self.boxes:
+        #     print(b["text"], b.get("layoutno"))
+        # print(tbls)
+
+        # return {
+        #     "title": title,
+        #     "authors": " ".join(authors),
+        #     "abstract": abstr,
+        #     "sections": [(b["text"] + self._line_tag(b, zoomin), b.get("layoutno", "")) for b in self.boxes[i:] if
+        #                  re.match(r"(text|title)", b.get("layoutno", "text"))],
+        #     "tables": tbls
+        # }
+
+       
+        sections = []
         for b in self.boxes:
             print(b["text"], b.get("layoutno"))
+            sections.append(b["text"])
         print(tbls)
 
-        return {
-            "title": title,
-            "authors": " ".join(authors),
-            "abstract": abstr,
-            "sections": [(b["text"] + self._line_tag(b, zoomin), b.get("layoutno", "")) for b in self.boxes[i:] if
-                         re.match(r"(text|title)", b.get("layoutno", "text"))],
-            "tables": tbls
-        }
+        return sections
+    
+
+    
+
+
+
 
 
 def chunk(filename, binary=None, from_page=0, to_page=100000,
@@ -280,74 +363,71 @@ def chunk(filename, binary=None, from_page=0, to_page=100000,
         Only pdf is supported.
         The abstract of the paper will be sliced as an entire chunk, and will not be sliced partly.
     """
+
     pdf_parser = None
     if re.search(r"\.pdf$", filename, re.IGNORECASE):
         if not kwargs.get("parser_config", {}).get("layout_recognize", True):
             pdf_parser = PlainParser()
-            paper = {
-                "title": filename,
-                "authors": " ",
-                "abstract": "",
-                "sections": pdf_parser(filename if not binary else binary, from_page=from_page, to_page=to_page)[0],
-                "tables": []
-            }
+            sections = pdf_parser(filename if not binary else binary, from_page=from_page, to_page=to_page)[0]
         else:
             pdf_parser = Pdf()
-            paper = pdf_parser(filename if not binary else binary,
+            sections = pdf_parser(filename if not binary else binary,
                                from_page=from_page, to_page=to_page, callback=callback)
     else:
         raise NotImplementedError("file type not supported yet(pdf supported)")
+    
 
-    doc = {"docnm_kwd": filename, "authors_tks": rag_tokenizer.tokenize(paper["authors"]),
-           "title_tks": rag_tokenizer.tokenize(paper["title"] if paper["title"] else filename)}
-    doc["title_sm_tks"] = rag_tokenizer.fine_grained_tokenize(doc["title_tks"])
-    doc["authors_sm_tks"] = rag_tokenizer.fine_grained_tokenize(doc["authors_tks"])
-    # is it English
-    eng = lang.lower() == "english"  # pdf_parser.is_english
-    print("It's English.....", eng)
+    return sections
 
-    res = tokenize_table(paper["tables"], doc, eng)
+    # doc = {"docnm_kwd": filename, "authors_tks": rag_tokenizer.tokenize(paper["authors"]),
+    #        "title_tks": rag_tokenizer.tokenize(paper["title"] if paper["title"] else filename)}
+    # doc["title_sm_tks"] = rag_tokenizer.fine_grained_tokenize(doc["title_tks"])
+    # doc["authors_sm_tks"] = rag_tokenizer.fine_grained_tokenize(doc["authors_tks"])
+    # # is it English
+    # eng = lang.lower() == "english"  # pdf_parser.is_english
+    # print("It's English.....", eng)
 
-    if paper["abstract"]:
-        d = copy.deepcopy(doc)
-        txt = pdf_parser.remove_tag(paper["abstract"])
-        d["important_kwd"] = ["abstract", "总结", "概括", "summary", "summarize"]
-        d["important_tks"] = " ".join(d["important_kwd"])
-        d["image"], poss = pdf_parser.crop(
-            paper["abstract"], need_position=True)
-        add_positions(d, poss)
-        tokenize(d, txt, eng)
-        res.append(d)
+    # res = tokenize_table(paper["tables"], doc, eng)
 
-    sorted_sections = paper["sections"]
-    # set pivot using the most frequent type of title,
-    # then merge between 2 pivot
-    # for i in sorted_sections:
-    #     print(i)
-    # print("&&&&&&&&&&&&&&&")
-    bull = bullets_category([txt for txt, _ in sorted_sections])
-    most_level, levels = title_frequency(bull, sorted_sections)
-    assert len(sorted_sections) == len(levels)
-    sec_ids = []
-    sid = 0
-    for i, lvl in enumerate(levels):
-        if lvl <= most_level and i > 0 and lvl != levels[i - 1]:
-            sid += 1
-        sec_ids.append(sid)
-        print(lvl, sorted_sections[i][0], most_level, sid)
+    # if paper["abstract"]:
+    #     d = copy.deepcopy(doc)
+    #     txt = pdf_parser.remove_tag(paper["abstract"])
+    #     d["important_kwd"] = ["abstract", "总结", "概括", "summary", "summarize"]
+    #     d["important_tks"] = " ".join(d["important_kwd"])
+    #     d["image"], poss = pdf_parser.crop(
+    #         paper["abstract"], need_position=True)
+    #     add_positions(d, poss)
+    #     tokenize(d, txt, eng)
+    #     res.append(d)
 
-    chunks = []
-    last_sid = -2
-    for (txt, _), sec_id in zip(sorted_sections, sec_ids):
-        if sec_id == last_sid:
-            if chunks:
-                chunks[-1] += "\n" + txt
-                continue
-        chunks.append(txt)
-        last_sid = sec_id
-    res.extend(tokenize_chunks(chunks, doc, eng, pdf_parser))
-    return res
+    # sorted_sections = paper["sections"]
+    # # set pivot using the most frequent type of title,
+    # # then merge between 2 pivot
+    # # for i in sorted_sections:
+    # #     print(i)
+    # # print("&&&&&&&&&&&&&&&")
+    # bull = bullets_category([txt for txt, _ in sorted_sections])
+    # most_level, levels = title_frequency(bull, sorted_sections)
+    # assert len(sorted_sections) == len(levels)
+    # sec_ids = []
+    # sid = 0
+    # for i, lvl in enumerate(levels):
+    #     if lvl <= most_level and i > 0 and lvl != levels[i - 1]:
+    #         sid += 1
+    #     sec_ids.append(sid)
+    #     print(lvl, sorted_sections[i][0], most_level, sid)
 
+    # chunks = []
+    # last_sid = -2
+    # for (txt, _), sec_id in zip(sorted_sections, sec_ids):
+    #     if sec_id == last_sid:
+    #         if chunks:
+    #             chunks[-1] += "\n" + txt
+    #             continue
+    #     chunks.append(txt)
+    #     last_sid = sec_id
+    # res.extend(tokenize_chunks(chunks, doc, eng, pdf_parser))
+    # return res
 
 """
     readed = [0] * len(paper["lines"])
@@ -428,6 +508,9 @@ def chunk(filename, binary=None, from_page=0, to_page=100000,
 """
 
 
+
+
+
 # if __name__ == "__main__":
 #     import sys
 
@@ -435,12 +518,6 @@ def chunk(filename, binary=None, from_page=0, to_page=100000,
 #         pass
 #     chunk(sys.argv[1], callback=dummy)
 
-
-def encode_image(image):
-    buffered = BytesIO()
-    image.save(buffered, format="JPEG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-    return img_str
 
 
 router = APIRouter()
@@ -465,13 +542,6 @@ async def chunk_endpoint(file: UploadFile = File(...), from_page: int = Form(0),
 
     result = chunk(filename, binary, from_page, to_page, lang, callback=callback)
 
-    # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
 
-    # for i in result:
-    #     print(i)
 
-    for item in result:
-        if 'image' in item:
-            item['image'] = encode_image(item['image'])
-
-    return JSONResponse(content={"result": result})
+    return JSONResponse(content={"sections": result})  # Modified to return boxes
